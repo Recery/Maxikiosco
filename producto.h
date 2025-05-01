c01edddfe6af738a9dd917527ad5de53b9bfabd5
Recery <142628253+Recery@users.noreply.github.com>
2025-05-01 10:59:07 -0300

    agregado producto

---
+++ producto.h
@@ -0,0 +1,45 @@
+#ifndef PRODUCTO_H_INCLUDED
+#define PRODUCTO_H_INCLUDED
+
+class Producto {
+private:
+    int numero;
+    char nombre[30];
+    char tipo[30];
+    int precio;
+    int stock;
+public:
+    void setNumero(int nuevoNumero) {
+        numero = nuevoNumero;
+    }
+    void setNombre(const char* nuevoNombre) {
+        nombre = nuevoNombre;
+    }
+    void setTipo(const char* nuevoTipo) {
+        tipo = nuevoTipo;
+    }
+    void setPrecio(int nuevoPrecio) {
+        precio = nuevoPrecio;
+    }
+    void setStock(int nuevoStock) {
+        stock = nuevoStock;
+    }
+
+    int getNumero() {
+        return numero;
+    }
+    const char* getNombre() {
+        return nombre;
+    }
+    const char* getTipo() {
+        return tipo;
+    }
+    int getPrecio() {
+        return precio;
+    }
+    int getStock() {
+        return stock;
+    }
+};
+
+#endif // PRODUCTO_H_INCLUDED
